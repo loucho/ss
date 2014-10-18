15d099f13c24df7af188952b3d55da66705ce8c3
loucho <loucho@redmantech.com>
2014-10-18 16:31:07 -0500

    cambio de las rutas de los partials

--- javascripts/angular-app/app.js
+++ javascripts/angular-app/app.js
@@ -4,13 +4,13 @@ var app = angular.module('app', ['ngRoute', 'ngAnimate', 'turnosControllers', 'S
 app.config(['$routeProvider', 'cfpLoadingBarProvider', function ($routeProvider, cfpLoadingBarProvider) {
     cfpLoadingBarProvider.includeSpinner = true;
     $routeProvider.when('/turnos/captura', {
-        templateUrl: '/partials/turnos/captura.html',
+        templateUrl: 'partials/turnos/captura.html',
         controller: 'CapturaTurnoController'
     }).when('/turnos/atencion', {
-        templateUrl: '/partials/turnos/atencion.html',
+        templateUrl: 'partials/turnos/atencion.html',
         controller: 'AtiendeTurnoController'
     }).when('/turnos/correccion', {
-        templateUrl: '/partials/turnos/correccion.html',
+        templateUrl: 'partials/turnos/correccion.html',
         controller: 'CorrigeTurnoController'
     }).otherwise({
         redirectTo: '/turnos/captura'
