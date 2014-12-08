556f5dcdee928434213f8b5a8f940fca25da36a7
loucho <loucho@redmantech.com>
2014-12-07 20:09:24 -0600

    usar todos los endpoints

--- javascripts/angular-app/controllers/turnosControllers.js
+++ javascripts/angular-app/controllers/turnosControllers.js
@@ -1,8 +1,8 @@
 var turnosControllers = angular.module('turnosControllers', []);
 
 turnosControllers.controller('CapturaTurnoController', ['$scope', '$http', 'dialogs', 'Priority', 'ProcessType',
-    'SenderType', 'Area', 'Institution', 'Position', 'Person',
-    function ($scope, $http, dialogs, Priority, ProcessType, SenderType, Area, Institution, Position, Person) {
+    'SenderType', 'Area', 'Institution', 'Position', 'Person', 'Organization', 'Turn',
+    function ($scope, $http, dialogs, Priority, ProcessType, SenderType, Area, Institution, Position, Person, Organization, Turn) {
         $scope.priorities = Priority.query();
         $scope.processTypes = ProcessType.query();
         $scope.today = new Date();
@@ -11,9 +11,11 @@ turnosControllers.controller('CapturaTurnoController', ['$scope', '$http', 'dial
         $scope.institutions = Institution.query();
         $scope.positions = Position.query();
         $scope.people = Person.query();
-        $scope.areas = Area.query();
+        $scope.organizations = Organization.query();
+        $scope.areas = Area.query({idDependencia: [0, 1]});
+        $scope.internalAreas = Area.query();
         $scope.files = [{"file": ""}];
-        $scope.turno = {areas: []};
+        $scope.turno = {areas: [], remitente: {}};
 
         $scope.checkedArea = function (code) {
             return _.contains($scope.turno.areas, code);
@@ -37,11 +39,22 @@ turnosControllers.controller('CapturaTurnoController', ['$scope', '$http', 'dial
             $scope.files.splice(i, 1);
         };
 
+        $scope.getPosition = function (idPerson) {
+            var item = _.findWhere($scope.people, {
+                id: idPerson
+            });
+            return item.cargoIes.nombre;
+        };
+
         $scope.getCode911 = function (institutionCode) {
             var institution = _.findWhere($scope.institutions, {
-                code: institutionCode
+                id: institutionCode
             });
-            return institution.code911;
+            return institution.clave911;
+        };
+
+        $scope.save = function () {
+            console.log(JSON.stringify($scope.turno));
         };
     }]);
 
--- javascripts/angular-app/resources/SEPServices.js
+++ javascripts/angular-app/resources/SEPServices.js
@@ -1,11 +1,11 @@
 var SEPServices = angular.module('SEPServices', ['ngResource', 'config']);
 
 SEPServices.factory('Priority', ['$resource', 'config', function ($resource, config) {
-    return $resource(config.fakeUrl + '/priorities.json', {}, {});
+    return $resource(config.apiUrl + '/prioridad', {}, {});
 }]);
 
 SEPServices.factory('ProcessType', ['$resource', 'config', function ($resource, config) {
-    return $resource(config.fakeUrl + '/process-types.json', {}, {});
+    return $resource(config.apiUrl + '/tipo-tramite', {}, {});
 }]);
 
 SEPServices.factory('SenderType', ['$resource', 'config', function ($resource, config) {
@@ -13,21 +13,25 @@ SEPServices.factory('SenderType', ['$resource', 'config', function ($resource, c
 }]);
 
 SEPServices.factory('Institution', ['$resource', 'config', function ($resource, config) {
-    return $resource(config.fakeUrl + '/institutions.json', {}, {});
+    return $resource(config.apiUrl + '/ies', {}, {});
+}]);
+
+SEPServices.factory('Organization', ['$resource', 'config', function ($resource, config) {
+    return $resource(config.apiUrl + '/organismo', {}, {});
 }]);
 
 SEPServices.factory('Position', ['$resource', 'config', function ($resource, config) {
-    return $resource(config.fakeUrl + '/positions.json', {}, {});
+    return $resource(config.apiUrl + '/cargo', {}, {});
 }]);
 
 SEPServices.factory('Person', ['$resource', 'config', function ($resource, config) {
-    return $resource(config.fakeUrl + '/people.json', {}, {});
+    return $resource(config.apiUrl + '/directorio-instituciones', {}, {});
 }]);
 
 SEPServices.factory('Area', ['$resource', 'config', function ($resource, config) {
-    return $resource(config.fakeUrl + '/areas.json', {}, {});
+    return $resource(config.apiUrl + '/area-interna', {}, {});
 }]);
 
 SEPServices.factory('Turn', ['$resource', 'config', function ($resource, config) {
-    return $resource(config.fakeUrl + '/turns.json', {}, {});
+    return $resource(config.apiUrl + '/turno', {}, {});
 }]);
