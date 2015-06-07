36170b6134d4f512b7b462f168a80e1678d3fff4
loucho <loucho@redmantech.com>
2015-06-07 18:15:40 -0500

    agregar otro recuadro para ver los turnos de todos los departamentos dependientes

--- javascripts/angular-app/controllers/homeController.js
+++ javascripts/angular-app/controllers/homeController.js
@@ -1,8 +1,30 @@
 var homeControllers = angular.module('homeControllers', []);
 
-homeControllers.controller('HomeController', ['$scope', 'Turn', 'Status', function ($scope, Turn, Status) {
+homeControllers.controller('HomeController', ['$scope', 'Turn', 'Status', 'Area', '$q', function ($scope, Turn, Status, Area, $q) {
+    var areas = [$scope.currentUser.idArea];
+    $scope.areas = Area.query({}, function () {
+        getDependantAreas($scope.currentUser.idArea);
+        function getDependantAreas(area) {
+            console.log('entrando para ver el area ' + area);
+            _.forEach($scope.areas, function (item) {
+                console.log('viendo el area ' + item.id + ' tiene dependencia: '+ item.dependencia);
+                if (item.dependencia == area) {
+                    areas.push(item.id);
+                    getDependantAreas(item.id);
+                }
+            })
+        }
+    });
     $scope.stati = Status.query();
-    $scope.chartConfig = {
+
+    $q.all([
+        $scope.areas.$promise,
+        $scope.stati.$promise
+    ]).then(function () {
+        loadDashboard();
+    });
+
+    $scope.ownChartConfig = {
         options: {
             chart: {
                 type: 'column'
@@ -15,7 +37,7 @@ homeControllers.controller('HomeController', ['$scope', 'Turn', 'Status', functi
             }
         },
         title: {
-            text: 'Desglose de turnos'
+            text: 'Desglose de turnos de esta Area'
         },
         loading: true,
         xAxis: {
@@ -25,12 +47,37 @@ homeControllers.controller('HomeController', ['$scope', 'Turn', 'Status', functi
             title: {text: 'Número de turnos'}
         }
     };
+
+    $scope.dependantChartConfig = {
+        options: {
+            chart: {
+                type: 'column'
+            },
+            tooltip: {
+                style: {
+                    padding: 10,
+                    fontWeight: 'bold'
+                }
+            }
+        },
+        title: {
+            text: 'Desglose de turnos de Areas dependientes'
+        },
+        loading: true,
+        xAxis: {
+            title: {text: 'Estatus'}
+        },
+        yAxis: {
+            title: {text: 'Número de turnos'}
+        }
+    };
+
     $scope.loading = {
         waiting: true,
         assigned: true,
-        notClosed: true
+        notClosed: true,
+        dependants: true
     };
-    loadDashboard();
 
     $scope.$on('event:status-changed', function () {
         loadDashboard();
@@ -67,18 +114,17 @@ homeControllers.controller('HomeController', ['$scope', 'Turn', 'Status', functi
         $scope.loading.assigned = false;
     }
 
-    function loadDashboard() {
+    function loadOwnArea() {
         $scope.waitingTurns = [];
         $scope.notClosedTurns = [];
         $scope.assignedTurns = [];
-        $scope.chartConfig.series = undefined;
-        $scope.chartConfig.xAxis.categories = undefined;
-        $scope.chartConfig.loading = true;
-        $scope.loading = {
-            waiting: true,
-            assigned: true,
-            notClosed: true
-        };
+        $scope.ownChartConfig.series = undefined;
+        $scope.ownChartConfig.xAxis.categories = undefined;
+        $scope.ownChartConfig.loading = true;
+        $scope.loading.waiting = true;
+        $scope.loading.assigned = true;
+        $scope.loading.notClosed = true;
+
         var chartTurns = Turn.query({
             estatus: [1, 3, 4, 6, 9, 10, 11],
             idAreaAsignada: $scope.currentUser.idArea
@@ -110,7 +156,7 @@ homeControllers.controller('HomeController', ['$scope', 'Turn', 'Status', functi
                 }
             });
 
-            $scope.chartConfig.series = [{
+            $scope.ownChartConfig.series = [{
                 data: conocimiento,
                 name: 'Para conocimiento',
                 color: '#ffb469'
@@ -119,8 +165,63 @@ homeControllers.controller('HomeController', ['$scope', 'Turn', 'Status', functi
                 name: 'Para trámite',
                 color: '#69b4ff'
             }];
-            $scope.chartConfig.xAxis.categories = categories;
-            $scope.chartConfig.loading = false;
+            $scope.ownChartConfig.xAxis.categories = categories;
+            $scope.ownChartConfig.loading = false;
         });
+    }
+
+    function loadDependantAreas() {
+        $scope.dependantTurns = [];
+        $scope.dependantChartConfig.series = undefined;
+        $scope.dependantChartConfig.xAxis.categories = undefined;
+        $scope.dependantChartConfig.loading = true;
+        $scope.loading.dependants = true;
+        var chartTurns = Turn.query({
+            estatus: [1, 3, 4, 6, 9, 10, 11],
+            idAreaAsignada: areas
+        }, function () {
+            $scope.dependantTurns = chartTurns;
+            var count = _.countBy(chartTurns, function (item) {
+                return item.idEstatus;
+            });
+            var tramite = [];
+            var conocimiento = [];
+            var categories = [];
+            $scope.stati.forEach(function (status) {
+                if (count[status.id]) {
+                    var i = _.indexOf(categories, status.descripcion);
+                    if (i == -1) {
+                        categories.push(status.descripcion);
+                        tramite.push(0);
+                        conocimiento.push(0);
+                        i = _.indexOf(categories, status.descripcion);
+                    }
+                    if (status.idElemento == 1) {
+                        tramite[i] += count[status.id];
+                    }
+                    if (status.idElemento == 2) {
+                        conocimiento[i] += count[status.id];
+                    }
+                }
+            });
+
+            $scope.dependantChartConfig.series = [{
+                data: conocimiento,
+                name: 'Para conocimiento',
+                color: '#ffb469'
+            }, {
+                data: tramite,
+                name: 'Para trámite',
+                color: '#69b4ff'
+            }];
+            $scope.dependantChartConfig.xAxis.categories = categories;
+            $scope.dependantChartConfig.loading = false;
+            $scope.loading.dependants = false;
+        });
+    }
+
+    function loadDashboard() {
+        loadOwnArea();
+        loadDependantAreas();
     }
 }]);
